8344604083e1b958c68a8e98d0222686e8eea5b2
abdullahalnoman003 <noman22622@gmail.com>
2025-08-20 12:33:06 +0600

    Problem 1

--- p1.sh
+++ p1.sh
@@ -1,1 +1,12 @@
+# Name: Abdullah AL Noman 
+# ID: 232-35-003 Section: 41_A2
 #!/bin/bash
+read -p "Enter a limit: " n
+sum=0
+
+for ((i=1; i<=n; i++))
+do
+sum=$((sum+i))
+done
+
+echo "Sum of numbers from 1 to $n is: $sum" 
