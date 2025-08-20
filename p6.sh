93c497664bc1fdbb83b4099368159bb21358a85e
abdullahalnoman003 <noman22622@gmail.com>
2025-08-20 23:15:37 +0600

    Problem 6

---
+++ p6.sh
@@ -0,0 +1,10 @@
+# Name: Abdullah Al Noman
+# ID: 232-35-003 Section: 41_A2
+
+#!/bin/bash
+echo "Enter 5 values:"
+read -a arr
+
+echo "Index 1 = ${arr[1]}"
+echo "Index 2 = ${arr[2]}"
+echo "Index 4 = ${arr[4]}"
