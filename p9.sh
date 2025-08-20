ebe2c65810fb6b9b33a4be880fd741ace649a321
abdullahalnoman003 <noman22622@gmail.com>
2025-08-20 23:48:34 +0600

    Problem 9

---
+++ p9.sh
@@ -0,0 +1,13 @@
+# Name: Abdullah Al Noman
+# ID: 232-35-003 Section: 41_A2
+
+#!/bin/bash
+read -p "Enter side a: " a
+read -p "Enter side b: " b
+read -p "Enter side c: " c
+
+if (( a+b>c && a+c>b && b+c>a )); then
+    echo "Valid Triangle"
+else
+    echo "Not a Valid Triangle"
+fi
