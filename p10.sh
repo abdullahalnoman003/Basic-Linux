7ba242ee55fa956ea4e7d46251e71079fc0a13cd
abdullahalnoman003 <noman22622@gmail.com>
2025-08-20 23:48:10 +0600

    Problem 10

---
+++ p10.sh
@@ -0,0 +1,24 @@
+# Name: Abdullah Al Noman
+# ID: 232-35-003 Section: 41_A2
+
+#!/bin/bash
+read -p "Enter first number: " a
+read -p "Enter second number: " b
+read -p "Enter third number: " c
+
+if((a>b));
+then
+    if((a>c));
+    then
+        echo "$a is the largest"
+    else
+        echo "$c is the largest"
+    fi
+else
+    if ((b>c));
+    then
+        echo "$b is the largest"
+    else
+        echo "$c is the largest"
+    fi
+fi
