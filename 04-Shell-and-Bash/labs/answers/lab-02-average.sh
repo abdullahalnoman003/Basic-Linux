65e611d2cdd81fd4981928672555e85d69244c10
abdullahalnoman003 <noman22622@gmail.com>
2026-09-09 15:58:11 +0600

    Add shell scripts for Lab 01, Lab 02, and Lab 03 — Sum, Average, and Leap Year calculations

---
+++ 04-Shell-and-Bash/labs/answers/lab-02-average.sh
@@ -0,0 +1,12 @@
+﻿
+#!/bin/bash
+read -p "Enter numbers with by space: " -a arr
+sum=0
+
+for num in "${arr[@]}"
+do
+    sum=$((sum+num))
+done
+size=${#arr[@]}
+avg=$((sum/size))
+echo "Average = $avg"
---
+++ 04-Shell-and-Bash/labs/answers/lab-03-leap-year.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+read -p "Enter years separated by space: " -a year
+
+for y in "${year[@]}"
+do
+    if (( (y % 400 == 0) || (y % 4 == 0 && y % 100 != 0) ))
+    then
+        echo "$y is a Leap Year"
+    else
+        echo "$y is not a Leap Year"
+    fi
+done
